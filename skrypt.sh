e391d9fdc1de12a0793e012c329c6022dcfb4104
Student DSW <studentdsw@Student.DSW>
2022-04-21 18:52:39 +0200

    zad3

--- skrypt.sh
+++ skrypt.sh
@@ -1,1 +1,22 @@
-date
+#!/bin/bash
+VAR1=$1
+VAR2="--date"
+VAR3="--logs"
+x=$2
+
+if [ "$VAR1" = "$VAR2" ]; then
+    date
+elif [  "$VAR1" = "$VAR3" ]
+then 
+	if [ -z $x ]
+	then
+		x=100
+	fi
+   for ((i=1; i<=x; i++))
+	do
+		name="log"$i".txt"
+		touch $name
+		echo "skrypt.sh" > $name
+		date >> $name
+	done
+fi
